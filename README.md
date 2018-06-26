9570bea6040b48aeaf40423224bd36c820925bae
Jafsari <justinafsari@yahoo.com>
2018-06-26 03:46:14 -0700

    changed styles with navbar to be red and white

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pokedex
 
-![screen shot 2018-06-26 at 3 30 08 am](https://user-images.githubusercontent.com/28635782/41905945-743cd156-78f1-11e8-85a2-ee8086dcac7a.png)
+![screen shot 2018-06-26 at 3 44 43 am](https://user-images.githubusercontent.com/28635782/41906688-5cacef9c-78f3-11e8-9d89-79f5ac5633dc.png)
--- pokemon/src/components/navbar.jsx
+++ pokemon/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import '../styles/navbar.css'
 
 export default class Navigation extends React.Component {
   constructor(props) {
@@ -19,16 +20,16 @@ export default class Navigation extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light>
+        <Navbar id="bg-light" color="light" light>
         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-        <NavbarBrand href="/" className="mr-auto white poke">Pokedex</NavbarBrand>
+        <NavbarBrand href="/" id ="bg-light" className="mr-auto white poke">Pokedex</NavbarBrand>
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
               <NavItem>
-                <NavLink >Pokemon</NavLink>
+                <NavLink id="NavColor" >Pokemon</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink >About</NavLink>
+                <NavLink id="NavColor" >About</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
